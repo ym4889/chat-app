9fea15ff26698b4c3cc1a4a37f12f35b6636c7f1
ym4889 <m.yamazaki4889@gmail.com>
2026-05-07 11:29:34 +0900

    ユーザー認証機能の実装

---
+++ src/main/java/in/tech_camp/chat_app/controller/UserController.java
@@ -0,0 +1,64 @@
+package in.tech_camp.chat_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import in.tech_camp.chat_app.entity.UserEntity;
+import in.tech_camp.chat_app.form.LoginForm;
+import in.tech_camp.chat_app.form.UserForm;
+import in.tech_camp.chat_app.repository.UserRepository;
+import in.tech_camp.chat_app.service.UserService;
+import lombok.AllArgsConstructor;
+
+
+@Controller
+@AllArgsConstructor
+public class UserController {
+
+  private final UserRepository userRepository;
+
+  private final UserService userService;
+
+
+  @GetMapping("/users/sign_up")
+  public String showSignUp(Model model){
+    model.addAttribute("userForm", new UserForm());
+    return "users/signUp";
+  }
+
+  @PostMapping("/user")
+  public String createUSer(@ModelAttribute("userForm") UserForm userForm, Model model){
+    UserEntity userEntity = new UserEntity();
+    userEntity.setName(userForm.getName());
+    userEntity.setEmail(userForm.getEmail());
+    userEntity.setPassword(userForm.getPassword());
+
+    try {
+      userService.createUserWithEncryptedPassword(userEntity);
+    } catch (Exception e) {
+      System.out.println("エラー : " + e);
+      model.addAttribute("userForm", userForm);
+      return "users/signUp";
+    }
+    return "redirect:/";
+  }
+  
+  @GetMapping("/users/login")
+  public String loginForm(Model model){
+    model.addAttribute("loginForm", new LoginForm());
+    return "users/login";
+  }
+  
+   @GetMapping("/login")
+  public String login(@RequestParam(value = "error", required = false) String error, @ModelAttribute("loginForm") LoginForm loginForm, Model model) {
+    if (error != null) {
+      model.addAttribute("loginError", "メールアドレスかパスワードが間違っています。");
+    }
+    return "users/login";
+  }
+  
+}
--- src/main/java/in/tech_camp/chat_app/form/UserForm.java
+++ src/main/java/in/tech_camp/chat_app/form/UserForm.java
@@ -1,11 +1,11 @@
-package in.tech_camp.chat_app.entity;
+package in.tech_camp.chat_app.form;
 
 import lombok.Data;
 
 @Data
-public class UserEntity {
-  private Integer id;
+public class UserForm {
   private String name;
   private String email;
   private String password;
+  private String passwordConfirmation;
 }
