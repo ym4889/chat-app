c5968b60efd12943089edd3bd75b353d5b1a0a6f
ym4889 <m.yamazaki4889@gmail.com>
2026-05-07 14:14:57 +0900

    ユーザー編集機能の実装

--- src/main/java/in/tech_camp/chat_app/controller/UserController.java
+++ src/main/java/in/tech_camp/chat_app/controller/UserController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import in.tech_camp.chat_app.entity.UserEntity;
 import in.tech_camp.chat_app.form.LoginForm;
+import in.tech_camp.chat_app.form.UserEditForm;
 import in.tech_camp.chat_app.form.UserForm;
 import in.tech_camp.chat_app.repository.UserRepository;
 import in.tech_camp.chat_app.service.UserService;
@@ -52,13 +54,42 @@ public class UserController {
     model.addAttribute("loginForm", new LoginForm());
     return "users/login";
   }
-  
+
    @GetMapping("/login")
   public String login(@RequestParam(value = "error", required = false) String error, @ModelAttribute("loginForm") LoginForm loginForm, Model model) {
     if (error != null) {
       model.addAttribute("loginError", "メールアドレスかパスワードが間違っています。");
     }
     return "users/login";
+  }
+  
+  @GetMapping("/users/{userId}/edit")
+  public String editUserForm(@PathVariable("userId") Integer userId, Model model) {
+    UserEntity user = userRepository.findById(userId);
+
+    UserEditForm userForm = new UserEditForm();
+    userForm.setId(user.getId());
+    userForm.setName(user.getName());
+    userForm.setEmail(user.getEmail());
+
+    model.addAttribute("user", userForm);
+    return "users/edit";
+  }
+
+  @PostMapping("/users/{userId}")
+  public String updateUser(@PathVariable("userId") Integer userId, @ModelAttribute("user") UserEditForm userEditForm, Model model) {
+    UserEntity user = userRepository.findById(userId);
+    user.setName(userEditForm.getName());
+    user.setEmail(userEditForm.getEmail());
+
+    try {
+      userRepository.update(user);
+    } catch (Exception e) {
+      System.out.println("エラー：" + e);
+      model.addAttribute("user", userEditForm);
+      return "users/edit";
+    }
+    return "redirect:/";
   }
   
 }
---
+++ src/main/java/in/tech_camp/chat_app/form/UserEditForm.java
@@ -0,0 +1,10 @@
+package in.tech_camp.chat_app.form;
+
+import lombok.Data;
+
+@Data
+public class UserEditForm {
+  private Integer id;
+  private String name;
+  private String email;
+}
