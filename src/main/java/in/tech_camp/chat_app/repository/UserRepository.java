92295d8122648726d38e9230dfc64eda0c1296e3
ym4889 <m.yamazaki4889@gmail.com>
2026-05-07 17:35:57 +0900

    バリデーションの設定

--- src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
@@ -22,7 +22,11 @@ public interface UserRepository {
 
   @Update("UPDATE users SET name = #{name}, email = #{email} WHERE id = #{id}")
   void update(UserEntity user);
-} 
 
- 
+  @Select("SELECT EXISTS(SELECT 1 FROM users WHERE email = #{email})")
+  boolean existsByEmail(String email);
+
+  @Select("SELECT COUNT(*) > 0 FROM users WHERE email = #{email} AND id != #{userId}")
+  boolean existsByEmailExcludingCurrent(String email, Integer userId);
+} 
  
