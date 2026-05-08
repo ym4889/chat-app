48ac13695cacfe5057cbd8f47e99026fc9a1d498
ym4889 <m.yamazaki4889@gmail.com>
2026-05-08 16:30:02 +0900

    チャットルーム作成機能の実装

--- src/main/java/in/tech_camp/chat_app/controller/RoomController.java
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -1,24 +1,36 @@
 package in.tech_camp.chat_app.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
+import in.tech_camp.chat_app.entity.RoomEntity;
+import in.tech_camp.chat_app.entity.RoomUserEntity;
 import in.tech_camp.chat_app.entity.UserEntity;
 import in.tech_camp.chat_app.form.RoomForm;
+import in.tech_camp.chat_app.repository.RoomRepository;
+import in.tech_camp.chat_app.repository.RoomUserRepository;
 import in.tech_camp.chat_app.repository.UserRepository;
+import in.tech_camp.chat_app.validation.ValidationOrder;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
 public class RoomController {
   private final UserRepository userRepository;
+  private final RoomRepository roomRepository;
+  private final RoomUserRepository roomUserRepository;
+
 
   @GetMapping("/rooms/new")
   public String showRoomNew(@AuthenticationPrincipal CustomUserDetail currentUser, Model model){
@@ -26,11 +38,51 @@ public class RoomController {
     model.addAttribute("users", users);
     model.addAttribute("roomForm", new RoomForm());
     return "rooms/new";
-  } 
+  }
 
   @PostMapping("/rooms")
-  public String createRoom(@ModelAttribute("RoomForm") RoomForm roomForm){
-    System.out.println("roomForm:"+ roomForm);
+  public String createRoom(@ModelAttribute("RoomForm") @Validated(ValidationOrder.class) RoomForm roomForm, BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetail currentUser, Model model){
+    if (bindingResult.hasErrors()) {
+      List<String> errorMessages = bindingResult.getAllErrors().stream()
+                              .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                              .collect(Collectors.toList());
+      List<UserEntity> users = userRepository.findAllExcept(currentUser.getId());
+      model.addAttribute("users", users);
+      model.addAttribute("roomForm", roomForm);
+      model.addAttribute("errorMessages", errorMessages);
+      return "rooms/new";
+    }
+
+    RoomEntity roomEntity = new RoomEntity();
+    roomEntity.setName(roomForm.getName());
+    try {
+      roomRepository.insert(roomEntity);
+    } catch (Exception e) {
+      System.out.println("エラー：" + e);
+      List<UserEntity> users = userRepository.findAllExcept(currentUser.getId());
+      model.addAttribute("users", users);
+      model.addAttribute("roomForm", new RoomForm());
+      return "rooms/new";
+    }
+
+    List<Integer> memberIds = roomForm.getMemberIds();
+    for (Integer userId : memberIds) {
+      UserEntity userEntity = userRepository.findById(userId);
+      RoomUserEntity roomUserEntity = new RoomUserEntity();
+      roomUserEntity.setRoom(roomEntity);
+      roomUserEntity.setUser(userEntity);
+
+      try {
+        roomUserRepository.insert(roomUserEntity);
+      } catch (Exception e) {
+        System.out.println("エラー：" + e);
+        List<UserEntity> users = userRepository.findAllExcept(currentUser.getId());
+        model.addAttribute("users", users);
+        model.addAttribute("roomForm", new RoomForm());
+        return "rooms/new";
+      }
+
+    }
     return "redirect:/";
   }
 }
