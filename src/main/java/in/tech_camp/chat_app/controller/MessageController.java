302b26b6dda160f148c714cedfda82ef3569208f
ym4889 <m.yamazaki4889@gmail.com>
2026-05-08 09:54:58 +0900

    対処

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -2,8 +2,8 @@ package in.tech_camp.chat_app.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
 import in.tech_camp.chat_app.entity.UserEntity;
@@ -22,6 +22,5 @@ public class MessageController {
     model.addAttribute("user", user);
     return "messages/index";
   }
-  
   
 }
