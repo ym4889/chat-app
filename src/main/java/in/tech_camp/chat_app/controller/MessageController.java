8eff56f13a53be885f41fa95f33f95e3b050e3d7
ym4889 <m.yamazaki4889@gmail.com>
2026-05-11 15:30:21 +0900

    画像保存の実装

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -1,5 +1,11 @@
 package in.tech_camp.chat_app.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,7 +19,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import in.tech_camp.chat_app.ImageUrl;
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
 import in.tech_camp.chat_app.entity.MessageEntity;
 import in.tech_camp.chat_app.entity.RoomEntity;
@@ -37,6 +45,8 @@ public class MessageController {
   private final RoomRepository roomRepository;
 
   private final MessageRepository messageRepository;
+
+  private final ImageUrl imageUrl;
 
   @GetMapping("/rooms/{roomId}/messages")
   public String showMessages(@PathVariable("roomId") Integer roomId,@AuthenticationPrincipal CustomUserDetail currentUser, Model model){
@@ -69,6 +79,20 @@ public class MessageController {
     
     MessageEntity message = new MessageEntity();
     message.setContent(messageForm.getContent());
+
+        MultipartFile imageFile = messageForm.getImage();
+    if (imageFile != null && !imageFile.isEmpty()) {
+      try {
+        String uploadDir = imageUrl.getImageUrl();
+        String fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + "_" + imageFile.getOriginalFilename();
+        Path imagePath = Paths.get(uploadDir, fileName);
+        Files.copy(imageFile.getInputStream(), imagePath);
+        message.setImage("/uploads/" + fileName);
+      } catch (IOException e) {
+        System.out.println("エラー：" + e);
+        return "redirect:/rooms/" + roomId + "/messages";
+      }
+    }
 
     UserEntity user = userRepository.findById(currentUser.getId());
     RoomEntity room =roomRepository.findById(roomId);
--- src/main/java/in/tech_camp/chat_app/form/MessageForm.java
+++ src/main/java/in/tech_camp/chat_app/form/MessageForm.java
@@ -1,5 +1,7 @@
 package in.tech_camp.chat_app.form;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import in.tech_camp.chat_app.validation.ValidationPriority1;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -10,4 +12,5 @@ public class MessageForm {
   
   private String content;
   
+  private MultipartFile image;
 }
