b7c2172cbf96cdb7196bf1441e17865985cd2ddd
ym4889 <m.yamazaki4889@gmail.com>
2026-05-11 11:11:54 +0900

    メッセージの表示

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -50,8 +50,11 @@ public class MessageController {
 
     model.addAttribute("messageForm", new MessageForm());
     model.addAttribute("roomId", roomId);
-        
+
+    List<MessageEntity> messages = messageRepository.findByRoomId(roomId);
+    model.addAttribute("messages", messages);
     return "messages/index";
+
   }
 
   @PostMapping("/rooms/{roomId}/messages")
