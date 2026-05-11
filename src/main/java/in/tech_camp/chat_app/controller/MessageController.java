81fdb114af0109ad236ad670a643658a2b5be1b0
ym4889 <m.yamazaki4889@gmail.com>
2026-05-11 13:01:26 +0900

    空のメッセージが送信できないバリデーションの設定

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -59,7 +61,11 @@ public class MessageController {
   }
 
   @PostMapping("/rooms/{roomId}/messages")
-  public String saveMessage(@PathVariable("roomId") Integer roomId, @ModelAttribute("messageForm") MessageForm messageForm, @AuthenticationPrincipal CustomUserDetail currentUser) {
+  public String saveMessage(@PathVariable("roomId") Integer roomId, @ModelAttribute("messageForm") @Validated(ValidationOrder.class) MessageForm messageForm, BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetail currentUser) {
+     if (bindingResult.hasErrors()) {
+      return "redirect:/rooms/" + roomId + "/messages";
+    }
+    
     MessageEntity message = new MessageEntity();
     message.setContent(messageForm.getContent());
 
--- src/main/java/in/tech_camp/chat_app/form/MessageForm.java
+++ src/main/java/in/tech_camp/chat_app/form/MessageForm.java
@@ -1,9 +1,13 @@
 package in.tech_camp.chat_app.form;
 
+import in.tech_camp.chat_app.validation.ValidationPriority1;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MessageForm {
+  @NotBlank(message = "Name can't be blank", groups = ValidationPriority1.class)
+  
   private String content;
   
 }
