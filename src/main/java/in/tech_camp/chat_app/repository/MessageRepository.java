b7bed47f90133e41c4d837c1aca759789b0a2170
ym4889 <m.yamazaki4889@gmail.com>
2026-05-11 10:20:26 +0900

    メッセージ送信機能の実装

--- src/main/java/in/tech_camp/chat_app/repository/MessageRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/MessageRepository.java
@@ -1,8 +1,14 @@
 package in.tech_camp.chat_app.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import in.tech_camp.chat_app.entity.MessageEntity;
 
 @Mapper
 public interface MessageRepository {
-
+  @Insert("INSERT INTO messages(content, user_id, room_id) VALUES (#{content}, #{user.id}, #{room.id})")
+  @Options(useGeneratedKeys = true, keyProperty =  "id")
+  void insert(MessageEntity messageEntity);
 }
